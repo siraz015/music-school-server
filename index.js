2648231252a393c9993dbaee5e80ffebe12d795d
siraz015 <siraj015213@gmail.com>
2023-06-09 12:37:15 +0600

    created get carts data

--- index.js
+++ index.js
@@ -39,15 +39,15 @@ async function run() {
         })
 
         // cart collection related API
-        // app.get('/carts', async (req, res) => {
-        //     const email = req.query.email;
-        //     if (!email) {
-        //         res.send([])
-        //     }
-        //     const query = { email: email };
-        //     const result = await cartCollection.find(query).toArray();
-        //     res.send(result);
-        // })
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
 
         app.post('/carts', async (req, res) => {
             const item = req.body;
